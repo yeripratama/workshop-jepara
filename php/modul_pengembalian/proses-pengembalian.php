2905ed9ea66e40d307e0df594e3eb84b104a473f
Yeri <yeri@jogja.camp>
2016-03-17 20:40:21 +0700

    Update stok buku di transaksi pengembalian.

--- php/modul_pengembalian/proses-pengembalian.php
+++ php/modul_pengembalian/proses-pengembalian.php
@@ -17,6 +17,15 @@ $query = "INSERT INTO kembali (pinjam_id, tgl_kembali, denda)
 
 $hasil = mysqli_query($db, $query);
 if ($hasil == true) {
+    // ambil buku_id berdasarkan pinjam_id
+    $q = "SELECT buku.buku_id FROM buku JOIN pinjam WHERE buku.buku_id = pinjam.buku_id";
+    $hasil = mysqli_query($db, $q);
+    $hasil = mysqli_fetch_assoc($hasil);
+    $buku_id = $hasil['buku_id'];
+
+    $q = "UPDATE buku SET buku_jumlah = buku_jumlah + 1 WHERE buku_id = $buku_id";
+    $update_jumlah = mysqli_query($db, $q);
+
     header('Location: ../modul_peminjaman/pinjam-data.php');
 } else {
     header('Location: pengembalian.php?id_pinjam='. $pinjam_id);
