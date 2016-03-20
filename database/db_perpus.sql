291748ed1c6344c87f14b1acdc6b0e471b7f3dc3
andhikayuana <andhikayuana@gmail.com>
2016-03-20 17:03:34 +0700

    perbaikan db

--- database/db_perpus.sql
+++ database/db_perpus.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.8.1
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 19, 2016 at 06:03 AM
+-- Generation Time: Mar 20, 2016 at 05:02 PM
 -- Server version: 5.5.47-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `db_perpus`
+-- Database: `workshop_perpus`
 --
 
 -- --------------------------------------------------------
@@ -27,12 +27,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `anggota` (
-`anggota_id` int(10) unsigned NOT NULL,
+  `anggota_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `anggota_nama` varchar(50) NOT NULL,
   `anggota_alamat` text NOT NULL,
   `anggota_jk` enum('L','P') NOT NULL,
-  `anggota_telp` varchar(14) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `anggota_telp` varchar(14) NOT NULL,
+  PRIMARY KEY (`anggota_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `anggota`
@@ -49,13 +50,15 @@ INSERT INTO `anggota` (`anggota_id`, `anggota_nama`, `anggota_alamat`, `anggota_
 --
 
 CREATE TABLE IF NOT EXISTS `buku` (
-`buku_id` int(10) unsigned NOT NULL,
+  `buku_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `buku_judul` varchar(50) NOT NULL,
   `kategori_id` int(11) unsigned NOT NULL,
   `buku_deskripsi` text,
   `buku_jumlah` int(11) unsigned NOT NULL,
-  `buku_cover` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `buku_cover` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`buku_id`),
+  KEY `kategori_id` (`kategori_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `buku`
@@ -64,7 +67,9 @@ CREATE TABLE IF NOT EXISTS `buku` (
 INSERT INTO `buku` (`buku_id`, `buku_judul`, `kategori_id`, `buku_deskripsi`, `buku_jumlah`, `buku_cover`) VALUES
 (1, 'fdgfhfdfhd', 2, 'dsfsujjyugfbfhfthfbnhgnh', 56, NULL),
 (3, 'HTML', 2, 'information. Installation. In order to install Laravel 5 Entrust, just add ... You can also publish the configuration for this package to further customize table names', 49, NULL),
-(4, 'PHP', 2, 'information. Installation. In order to install Laravel 5 Entrust, just add ... You can also publish the configuration for this package to further customize table names', 52, NULL);
+(4, 'PHP', 2, 'information. Installation. In order to install Laravel 5 Entrust, just add ... You can also publish the configuration for this package to further customize table names', 51, NULL),
+(5, 'Buku baru', 10, 'Ini buku baru', 10, 'poto.jpg'),
+(7, 'preketek', 10, 'pro', 1, '15996662264_4088cd1e53_o.png');
 
 -- --------------------------------------------------------
 
@@ -73,19 +78,18 @@ INSERT INTO `buku` (`buku_id`, `buku_judul`, `kategori_id`, `buku_deskripsi`, `b
 --
 
 CREATE TABLE IF NOT EXISTS `kategori` (
-`kategori_id` int(10) unsigned NOT NULL,
-  `kategori_nama` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `kategori_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `kategori_nama` varchar(50) NOT NULL,
+  PRIMARY KEY (`kategori_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `kategori`
 --
 
 INSERT INTO `kategori` (`kategori_id`, `kategori_nama`) VALUES
-(1, 'Naovelaaas'),
 (2, 'Sci & Fic'),
-(4, 'Komikaaa'),
-(8, 'dfgdf');
+(10, 'Pemrograman');
 
 -- --------------------------------------------------------
 
@@ -94,11 +98,13 @@ INSERT INTO `kategori` (`kategori_id`, `kategori_nama`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `kembali` (
-`kembali_id` int(11) unsigned NOT NULL,
+  `kembali_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `pinjam_id` int(11) unsigned NOT NULL,
   `tgl_kembali` date NOT NULL,
-  `denda` double unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `denda` double unsigned NOT NULL,
+  PRIMARY KEY (`kembali_id`),
+  KEY `pinjam_id` (`pinjam_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `kembali`
@@ -106,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `kembali` (
 
 INSERT INTO `kembali` (`kembali_id`, `pinjam_id`, `tgl_kembali`, `denda`) VALUES
 (1, 5, '2016-03-31', 0),
-(2, 9, '2016-03-17', 0);
+(4, 12, '2016-03-20', 4000);
 
 -- --------------------------------------------------------
 
@@ -115,11 +121,12 @@ INSERT INTO `kembali` (`kembali_id`, `pinjam_id`, `tgl_kembali`, `denda`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `petugas` (
-`petugas_id` int(10) unsigned NOT NULL,
+  `petugas_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `petugas_nama` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`petugas_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `petugas`
@@ -135,12 +142,15 @@ INSERT INTO `petugas` (`petugas_id`, `petugas_nama`, `username`, `password`) VAL
 --
 
 CREATE TABLE IF NOT EXISTS `pinjam` (
-`pinjam_id` int(10) unsigned NOT NULL,
+  `pinjam_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `buku_id` int(11) unsigned NOT NULL,
   `anggota_id` int(11) unsigned NOT NULL,
   `tgl_pinjam` date NOT NULL,
-  `tgl_jatuh_tempo` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `tgl_jatuh_tempo` date NOT NULL,
+  PRIMARY KEY (`pinjam_id`),
+  KEY `anggota_id` (`anggota_id`),
+  KEY `buku_id` (`buku_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `pinjam`
@@ -148,82 +158,10 @@ CREATE TABLE IF NOT EXISTS `pinjam` (
 
 INSERT INTO `pinjam` (`pinjam_id`, `buku_id`, `anggota_id`, `tgl_pinjam`, `tgl_jatuh_tempo`) VALUES
 (5, 3, 1, '2016-03-11', '2016-03-19'),
-(9, 4, 2, '2016-03-18', '2016-03-31');
+(10, 3, 2, '2016-03-20', '2016-03-20'),
+(11, 4, 2, '2016-03-20', '2016-03-24'),
+(12, 3, 1, '2016-03-01', '2016-03-18');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `anggota`
---
-ALTER TABLE `anggota`
- ADD PRIMARY KEY (`anggota_id`);
-
---
--- Indexes for table `buku`
---
-ALTER TABLE `buku`
- ADD PRIMARY KEY (`buku_id`), ADD KEY `kategori_id` (`kategori_id`);
-
---
--- Indexes for table `kategori`
---
-ALTER TABLE `kategori`
- ADD PRIMARY KEY (`kategori_id`);
-
---
--- Indexes for table `kembali`
---
-ALTER TABLE `kembali`
- ADD PRIMARY KEY (`kembali_id`), ADD KEY `pinjam_id` (`pinjam_id`);
-
---
--- Indexes for table `petugas`
---
-ALTER TABLE `petugas`
- ADD PRIMARY KEY (`petugas_id`);
-
---
--- Indexes for table `pinjam`
---
-ALTER TABLE `pinjam`
- ADD PRIMARY KEY (`pinjam_id`), ADD KEY `anggota_id` (`anggota_id`), ADD KEY `buku_id` (`buku_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `anggota`
---
-ALTER TABLE `anggota`
-MODIFY `anggota_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `buku`
---
-ALTER TABLE `buku`
-MODIFY `buku_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `kategori`
---
-ALTER TABLE `kategori`
-MODIFY `kategori_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `kembali`
---
-ALTER TABLE `kembali`
-MODIFY `kembali_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `petugas`
---
-ALTER TABLE `petugas`
-MODIFY `petugas_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `pinjam`
---
-ALTER TABLE `pinjam`
-MODIFY `pinjam_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
@@ -232,20 +170,20 @@ MODIFY `pinjam_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 -- Constraints for table `buku`
 --
 ALTER TABLE `buku`
-ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`kategori_id`);
+  ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`kategori_id`);
 
 --
 -- Constraints for table `kembali`
 --
 ALTER TABLE `kembali`
-ADD CONSTRAINT `kembali_ibfk_1` FOREIGN KEY (`pinjam_id`) REFERENCES `pinjam` (`pinjam_id`);
+  ADD CONSTRAINT `kembali_ibfk_1` FOREIGN KEY (`pinjam_id`) REFERENCES `pinjam` (`pinjam_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `pinjam`
 --
 ALTER TABLE `pinjam`
-ADD CONSTRAINT `pinjam_ibfk_2` FOREIGN KEY (`anggota_id`) REFERENCES `anggota` (`anggota_id`),
-ADD CONSTRAINT `pinjam_ibfk_1` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`);
+  ADD CONSTRAINT `pinjam_ibfk_1` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pinjam_ibfk_2` FOREIGN KEY (`anggota_id`) REFERENCES `anggota` (`anggota_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
